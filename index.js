6bc14ba6d8edc316c4b710b6bd25975943c884d2
DanishAhmed-5 <da2022041@gmail.com>
2025-12-23 14:09:49 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+/*Question NO 1|/** */
+var students = [];
+students.push("Asim");
+students.push("Khizr");
+students.push("Orhan");
+
+console.log(students);
+
+//Question NO 2*/
+
+var fruits = ["Apple", "Banana", "Mango", "Orange"];
+console.log(fruits);
+
+//qusetion No 3
+
+var numbers = [10, 20, 30, 40, 50];
+console.log(numbers);
+
+
+//qusetion No 4
+
+var boolArray = [true, false, true, false];
+console.log(boolArray);
+
+//Question No 5
+
+var mixedArray = ["Ali", 25, true, null];
+console.log(mixedArray);
+
+//Question No 8
+
+var colors = ["Red", "Green", "Blue"];
+colors.unshift("Yellow"); 
+colors.push("Black");     
+
+console.log(colors);
+
+//question No 9
+
+var cities = ["Karachi", "Lahore", "Islamabad", "Quetta"];
+cities.pop();   
+
+cities.shift(); 
+
+console.log(cities);
+
+
+
+
+
